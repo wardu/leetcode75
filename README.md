41d1c0031180e2afb8235a0565caf08c423d9b0c
Warren Dubery <warrendubery@protonmail.ch>
2022-11-15 13:11:39 +0000

    File names and add README

---
+++ README.md
@@ -0,0 +1,1 @@
+Here are my solutions to the Leetcode 75 day challenge along with associated tests written with Jest.
--- day-01/runningSum.test-1480.js
+++ day-01/runningSum.test-1480.js
@@ -1,4 +1,4 @@
-const runningSum = require("./runningSum");
+const runningSum = require("./runningSum-1480");
 
 test("Gives the running sum of an array", () => {
   expect(runningSum([1, 2, 3, 4])).toEqual([1, 3, 6, 10]);
